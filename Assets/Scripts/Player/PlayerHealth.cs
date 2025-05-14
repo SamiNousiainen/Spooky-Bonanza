52187f382ae4e8cb5c50de944c549fab92d796fc
Hipitsu <t2ukhi00@students.oamk.fi>
2025-05-14 09:57:09 +0300

    creating scripts for the player's health system

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] private float maxHealth = 3f;
+    [SerializeField] private float currentHealth;
+
+    void Awake()
+    {
+        currentHealth = maxHealth;
+    } // Awake
+
+    public void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+
+        if (currentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+} // Class
