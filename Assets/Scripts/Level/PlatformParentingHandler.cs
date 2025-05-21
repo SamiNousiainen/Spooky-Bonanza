6958e90451d8069ec961cef57c2b60239b6948c0
Soffffe <iida_hamalainen@hotmail.com>
2025-05-21 10:46:36 +0300

    Level updates, PlatformParentingHandler fix

--- Assets/Scripts/Level/PlatformParentingHandler.cs
+++ Assets/Scripts/Level/PlatformParentingHandler.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 public class PlatformParentingHandler : MonoBehaviour {
 
     private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player"))
         other.transform.SetParent(transform);
     }
 
     private void OnTriggerExit(Collider other) {
-        other.transform.SetParent(null);
+        if (other.CompareTag("Player"))
+            other.transform.SetParent(null);
     }
 }
