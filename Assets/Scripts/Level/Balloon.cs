ae4623921cd94f5997a1dda7f508625d9ce98c38
SamiNousiainen <n1nosa00@students.oamk.fi>
2025-06-03 19:16:15 +0300

    Balloons can be popped by attacking

--- Assets/Scripts/Level/Balloon.cs
+++ Assets/Scripts/Level/Balloon.cs
@@ -1,21 +1,34 @@
 using UnityEngine;
 
-public class Balloon : MonoBehaviour {
+public class Balloon : MonoBehaviour, IDamageable {
 
     [SerializeField] private GameObject popVFX;
     [SerializeField] private GameObject balloon;
+    [SerializeField] private float hitPoints = 1f;
+
+    public bool HasTakenDamage { get; set; }
 
     private void OnTriggerEnter(Collider other) {
         if (other.CompareTag("Player")) {
-            balloon.SetActive(false);
-            popVFX.SetActive(true);
-            SoundManager.instance.PlaySFX(SFXType.Balloon, transform, 0.8f);
+            Pop();           
             other.GetComponent<PlayerMovement>().LaunchPlayer(Vector3.up * 5f);
         }
     }
 
     private void OnCollisionEnter(Collision collision) {
+        Pop();
+    }
+
+    public void TakeDamage(float damage) {
+        hitPoints -= damage;
+        if (hitPoints <= 0f) {
+            Pop();
+        }
+    }
+
+    private void Pop() {
         balloon.SetActive(false);
         popVFX.SetActive(true);
+        SoundManager.instance.PlaySFX(SFXType.Balloon, transform, 0.8f);
     }
 }
