3ad194862aabbc8c2ee0ef8d0aee985ca04b27fc
Hipitsu <t2ukhi00@students.oamk.fi>
2025-06-10 12:04:39 +0300

    there is now cooldown added for glide and block to activate after knockback

--- Assets/Scripts/Enemies/EnemyHurtBox.cs
+++ Assets/Scripts/Enemies/EnemyHurtBox.cs
@@ -1,16 +1,19 @@
 using UnityEngine;
 
-public class EnemyHurtBox : MonoBehaviour {
+public class EnemyHurtBox : MonoBehaviour
+{
 
     [SerializeField] private float knockbackForce = 10f;
     [SerializeField] private float damage = 1f;
 
-    private void OnTriggerEnter(Collider other) {
-        if (other.CompareTag("Player")) {
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
             SoundManager.instance.PlaySFX(SFXType.PlayerTakeDamage, Player.instance.transform, 0.8f);
             Vector3 knockbackDirection = other.transform.position - transform.position;
             other.gameObject.GetComponent<PlayerMovement>().Knockback(new Vector3(knockbackDirection.x, 1f, knockbackDirection.z) * knockbackForce);
-            //other.gameObject.GetComponent<PlayerCombat>() laita glide cooldownille
+            other.gameObject.GetComponent<PlayerCombat>().GlideCooldown();
             other.gameObject.GetComponent<PlayerHealth>().TakeDamage(damage);
         }
     }
