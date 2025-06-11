785fece2353cb3242614e98a8be7f48a4be23653
SamiNousiainen <sami.nousiainen13@gmail.com>
2025-06-11 11:00:53 +0300

    Small gravity fix + code cleanup

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -166,7 +166,6 @@ public class PlayerMovement : MonoBehaviour
         if (isGrounded == false) // Vain ilmassa
         {
             RaycastHit hit;
-            //int layerMask = ~(1 << LayerMask.NameToLayer("Collectible"));
 
             // Tarkistetaan, onko pää osunut kattoon
             if (Physics.SphereCast(transform.position, characterController.radius, Vector3.up, out hit, 0.3f, LayerMask.GetMask("Walkable Surface")))
@@ -188,7 +187,7 @@ public class PlayerMovement : MonoBehaviour
 
         if (isGrounded == true && velocity.y < 0f)
         {
-            velocity.y = groundedGravity;
+            characterController.Move(new Vector3(0f, groundedGravity, 0f));
         }
         else if (isFalling)
         {
@@ -206,7 +205,6 @@ public class PlayerMovement : MonoBehaviour
             velocity.y = nextYVelocity;
         }
 
-        //velocity.y += playerProperties.gravityModifier * Time.deltaTime;
         characterController.Move(new Vector3(0f, velocity.y + externalVelocity.y, 0f) * Time.deltaTime);
     } //HandleGravity
 
@@ -247,107 +245,3 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 }
-
-
-/*void OnMovementInput(InputAction.CallbackContext context)
-{
-    currentMovementInput = context.ReadValue<Vector2>();
-    currentMovement.x = currentMovementInput.x;
-    currentMovement.z = currentMovementInput.y;
-    isMovementPressed = currentMovementInput.x != 0 || currentMovementInput.y != 0;
-} // OnMovementInput*/
-
-
-/*using KBCore.Refs;
-using UnityEngine;
-
-[RequireComponent(typeof(CharacterController))]
-public class PlayerMovement : MonoBehaviour {
-
-    [SerializeField] private InputReader inputReader;
-    [SerializeField] private Transform cameraTransform;
-    [SerializeField, Self] private CharacterController characterController;
-    [SerializeField] private PlayerProperties playerProperties;
-
-    [Header("Ground Check")]
-    [SerializeField] private Transform groundCheckTransform;
-    [SerializeField] private float groundCheckRadius = 0.2f;
-
-    private Vector3 velocity;
-    private bool isGrounded;
-
-    private void OnValidate() {
-        this.ValidateRefs();
-    }
-
-    private void Awake() {
-        if (cameraTransform == null && Camera.main != null) {
-            cameraTransform = Camera.main.transform;
-        }
-    }
-
-    private void Update() {
-        HandleMovement();
-        HandleJump();
-        ApplyGravity();
-        GroundCheck();
-        RoofCheck();
-    }
-
-    private void HandleMovement() {
-        Vector2 input = inputReader.MoveInput;
-
-        //Convert input into camera-relative direction
-        Vector3 forward = cameraTransform.forward;
-        Vector3 right = cameraTransform.right;
-        forward.y = 0f;
-        right.y = 0f;
-        forward.Normalize();
-        right.Normalize();
-
-        Vector3 direction = (right * input.x + forward * input.y).normalized;
-        Vector3 horizontalVelocity = direction * playerProperties.moveSpeed;
-
-        //Apply horizontal movement only
-        characterController.Move(horizontalVelocity * Time.deltaTime);
-
-        if (direction.sqrMagnitude > 0f) {
-            Quaternion targetRotation = Quaternion.LookRotation(direction);
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 10f * Time.deltaTime);
-        }
-    }
-
-    private void HandleJump() {
-        if (inputReader.ConsumeJumpInput() == true && isGrounded == true) {
-            velocity.y = Mathf.Sqrt(playerProperties.jumpHeight * -2f * playerProperties.gravityModifier);
-        }
-    }
-
-    public void LaunchPlayer(float jumpForce) {
-        velocity.y = jumpForce;
-    }
-
-     private void ApplyGravity() {
-         if (isGrounded == true && velocity.y < 0f) {
-             velocity.y = -2f;
-         }
-
-         velocity.y += playerProperties.gravityModifier * Time.deltaTime;
-         characterController.Move(new Vector3(0f, velocity.y, 0f) * Time.deltaTime);
-     }
-
-     private void GroundCheck() {
-         isGrounded = Physics.CheckSphere(groundCheckTransform.position, groundCheckRadius, LayerMask.GetMask("Walkable Surface"));
-     }
-
-     private void RoofCheck() {
-         RaycastHit hit;
-         if (Physics.Raycast(transform.position, Vector3.up, out hit, 0.6f)) {
-             velocity.y = -2f;
-         }
-     }
-
-     private void OnDrawGizmos() {
-         Gizmos.DrawWireSphere(groundCheckTransform.position, groundCheckRadius);
-     }
-}*/
