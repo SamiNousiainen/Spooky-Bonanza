be38c4125b67b9c0054bc3f2044ef15ae5cb65a4
SamiNousiainen <n1nosa00@students.oamk.fi>
2025-06-11 08:20:51 +0300

    Pöytä fix ehkä

--- Assets/Scripts/Level/PlatformParentingHandler.cs
+++ Assets/Scripts/Level/PlatformParentingHandler.cs
@@ -3,8 +3,19 @@ using UnityEngine;
 public class PlatformParentingHandler : MonoBehaviour {
 
     private void OnTriggerEnter(Collider other) {
-        if (other.CompareTag("Player"))
-        other.transform.SetParent(transform);
+        if (other.CompareTag("Player")) {
+            if (other.transform.parent == null) {
+                other.transform.SetParent(transform);
+            }
+        }
+    }
+
+    private void OnTriggerStay(Collider other) {
+        if (other.CompareTag("Player")) {
+            if (other.transform.parent == null) {
+                other.transform.SetParent(transform);
+            }
+        }
     }
 
     private void OnTriggerExit(Collider other) {
